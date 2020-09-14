266fbd310232a97db9a083736c452919517778cd
Deirdris <spookyguu@gmail.com>
2020-09-14 17:43:52 +0200

    Check all, esc to cancel editing, counter, buttons to filter todos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,18 +10,46 @@
     </div>
 
     <div id="todos" >
-      <div v-for="(todo,i) in todos" :key="i" style="text-align: center">
+      <div v-for="(todo,i) in todosFilter" :key="i" style="text-align: center; padding-bottom: 30px;">
 
-       <span v-if="!todo.editing" class="todo-item-label" @dblclick="editTodo(todo)">
+        <span style="padding-right: 20px;">
+         <input type="checkbox" id="done" v-model="todo.completed">
+        </span>
+
+       <span v-if="!todo.editing" class="todo-item-label" @dblclick="editTodo(todo)" :class="{completed : todo.completed}">
        {{todo.title}}
        </span>
 
-       <input v-else class="todo-item-edit" type="text" v-model="todo.title" @blur="doneEditing(todo)" @keyup.enter="doneEditing(todo)">
+       <input v-else class="todo-item-edit" type="text" v-model="todo.title" @blur="doneEditing(todo)" @keyup.enter="doneEditing(todo)" @keyup.esc="cancelEditing(todo)">
 
          <button id="remove" v-on:click="removeTodo(i)"><i class="fas fa-times"></i></button>
 
       </div>
     </div>
+
+    <div style="display: flex; align-items: center; justify-content: center">
+
+      <div class="extras">
+
+        <label><input type="checkbox" :checked="!allDone" @change="checkAll">Check all</label>
+        <span>{{remaining}} items left</span>
+
+      </div>
+
+    </div>
+
+    <div style="display: flex; align-items: center; justify-content: center">
+
+    <div class="extras">
+
+        <button :class="{active: filter === 'all'}" @click="filter = 'all'">All</button>
+        <button :class="{active: filter === 'active'}" @click="filter = 'active'">Active</button>
+        <button :class="{active: filter === 'completed'}" @click="filter = 'completed'">Completed</button>
+
+    </div>
+
+  </div>
+
   </div>
 </template>
 
@@ -35,6 +63,8 @@ export default {
   data() {
     return {
       newTodo: '',
+      beforeEdit: '',
+      filter: 'all',
       todos:[
 
       ]
@@ -59,12 +89,41 @@ export default {
       this.todos.splice(i,1)
     },
     editTodo(todo){
+      this.beforeEdit = todo.title;
       todo.editing = true;
     },
     doneEditing(todo){
+      if(todo.title.length === 0){
+        todo.title = this.beforeEdit;
+      }
       todo.editing = false;
+    },
+    cancelEditing(todo){
+      todo.title = this.beforeEdit;
+      todo.editing = false;
+    },
+    checkAll(){
+      this.todos.forEach(todo => todo.completed = event.target.checked)
     }
 
+
+  },
+  computed: {
+    remaining(){
+      return this.todos.filter(todo => !todo.completed).length;
+    },
+    allDone(){
+      return this.remaining !== 0;
+    },
+    todosFilter(){
+      if(this.filter === 'all'){
+        return this.todos;
+      } else if (this.filter === 'active'){
+        return this.todos.filter(todo => !todo.completed);
+      } else if(this.filter === 'completed'){
+        return this.todos.filter(todo => todo.completed);
+      }
+    }
   }
 
 }
@@ -151,6 +210,46 @@ export default {
    color: #18189e;
    &:focus{
      outline: none;
+   }
+ }
+
+ .completed{
+   text-decoration: line-through;
+   color: dimgray;
+ }
+
+ .extras{
+   width: 30%;
+   display: flex;
+   align-items: center;
+   justify-content: space-between;
+   font-size: 20px;
+   border-top: 3px solid darkblue;
+   padding-top: 20px;
+   margin-bottom: 20px;
+   color: #18189e;
+   font-family: 'Dosis', sans-serif;
+
+   button{
+     font-size: 20px;
+     background-color: whitesmoke;
+     appearance: none;
+     border: none;
+     border-radius: 5px;
+     padding: 5px;
+     font-family: 'Dosis', sans-serif;
+     color: #18189e;
+
+     &:hover{
+       background: cornflowerblue;
+     }
+     &:focus{
+       outline: none;
+     }
+   }
+
+   .active{
+     background: darken(cornflowerblue,20%);
    }
  }
 
